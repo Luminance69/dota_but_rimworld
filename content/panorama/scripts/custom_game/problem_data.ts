8f651e3269071c1373b90107f9914828f054230a
Blasphemy-Incarnate <senyksia@gmail.com>
2022-01-11 22:34:28 +1100

    Add MinorBreak problem type

--- content/panorama/scripts/custom_game/problem_data.ts
+++ content/panorama/scripts/custom_game/problem_data.ts
@@ -13,9 +13,27 @@ const ProblemData =
             },
             repeat:
             {
-                1: "<br> - <span color='#d09b61'>{t}</span>",
+                1: "<br>    - <span color='#d09b61'>{t}</span>",
             },
         },
-        major: true
-    }
+        major: true,
+    },
+
+    MinorBreak:
+    {
+        name: "Minor break risk{xn}",
+        description:
+        {
+            main:
+            {
+                0: "These heroes are in a poor mood and may have a minor mental break at any time:<br>",
+                2: "<br><br>Check these heroes' needs to see what's bothering them.",
+            },
+            repeat:
+            {
+                1: "<br>    - <span color='#d09b61'>{t}</span>",
+            },
+        },
+        major: false,
+    },
 }
