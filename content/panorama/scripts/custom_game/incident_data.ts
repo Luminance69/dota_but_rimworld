8c151fb3428c01023c8f6f73da4c7ea94ddedb76
Senyksia <senyksia@gmail.com>
2022-01-15 18:27:34 +1100

    Populate letter data
    Added:
     - Cold snap
     - Creep disease
     - Gift
     - Hero sickness*
     - Invert day
     - Mad neutral
     - Mass neutral insanity
     - Psychic soothe/drone
     - Zzzztt
    
    * Currently supports generic text for all diseases. Possibly add
      specific flavour text for contageon etc.
    
    I'd like to add more dota specific flavour in future, but that should
    wait until implementation is figured out.

--- content/panorama/scripts/custom_game/incident_data.ts
+++ content/panorama/scripts/custom_game/incident_data.ts
@@ -36,19 +36,42 @@ const IncidentSeverity =
         tGlow: 15,
         tBounce: 5,
     },
+
+    PsychicSoothe:
+    {
+        color: "#79afdb",
+        sounds: ["LetterArriveGood", "PsychicSoothe"],
+        tGlow: 90,
+    },
+
+    PsychicDrone:
+    {
+        color: "#ccc47f",
+        sounds: ["LetterArriveBadUrgentSmall", "PsychicDrone"],
+        tGlow: 15,
+    },
+
+    MassInsanity:
+    {
+        color: "#ca7471",
+        sounds: ["LetterArriveBadUrgentBig", "PsychicDrone"],
+        tGlow: 6,
+        tBounce: 5,
+    },
 };
 
 const IncidentTypes =
 {
     ColdSnap:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Cold snap",
+        severity: IncidentSeverity.Special,
         description:
         {
             main:
             {
-                0: ""
+                0: "An unusual cold snap has set in.",
+                1: "<br><br>Cold snaps can quickly kill by hypothermia. Be sure to stay warm by seeking shelter near your buildings and Ancient.",
             },
             repeat: {},
         },
@@ -56,13 +79,13 @@ const IncidentTypes =
 
     CreepDisease:
     {
-        name: "Creep Disease",
-        severity: IncidentSeverity.Bad,
+        name: "Creep disease",
+        severity: IncidentSeverity.Special,
         description:
         {
             main:
             {
-                0: "Many of your creeps have been infected with a deadly illness."
+                0: "Many of your creeps have been infected with a deadly illness.",
             },
             repeat: {},
         },
@@ -70,13 +93,14 @@ const IncidentTypes =
 
     Gift:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Gift from {Patron}",
+        severity: IncidentSeverity.Good,
         description:
         {
             main:
             {
-                0: ""
+                0: "Benefactors from <span color='#01b8f5'>{patron}</span> have left you a gift:",
+                1: "<br><br>&nbsp;&nbsp;- {gift}",
             },
             repeat: {},
         },
@@ -84,27 +108,32 @@ const IncidentTypes =
 
     HeroSickness:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Disease: {Disease}",
+        severity: IncidentSeverity.Bad,
         description:
         {
             main:
             {
-                0: ""
+                0: "{n} of your heroes have gotten sick from {disease}.",
+                1: "<br><br>The following heroes have gotten sick:<br>",
             },
-            repeat: {},
+            repeat:
+            {
+                2: "<br>&nbsp;&nbsp;- <span color: '#bf8f5b'>{t}</span>",
+            },
         },
     },
 
     InvertDay:
     {
-        name: "",
+        name: "Solar eclipse",
         severity: IncidentSeverity.Neutral,
         description:
         {
             main:
             {
-                0: ""
+                0: "A rogue moon has passed in front of your sun, causing the battlefield to be engulfed in darkness for a short time.",
+                1: "<br><br>The eclipse is expected to last for {duration} days.",
             },
             repeat: {},
         },
@@ -112,13 +141,13 @@ const IncidentTypes =
 
     MadNeutral:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Mad {neutral}",
+        severity: IncidentSeverity.Special,
         description:
         {
             main:
             {
-                0: ""
+                0: "A local {neutral} has gone mad. It will attack everyone it sees.",
             },
             repeat: {},
         },
@@ -126,13 +155,14 @@ const IncidentTypes =
 
     MassNeutralInsanity:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Mad {neutral}s",
+        severity: IncidentSeverity.MassInsanity,
         description:
         {
             main:
             {
-                0: ""
+                0: "Some sort of psychic wave has swept over the landscape. Your heroes are okay, but...",
+                1: "<br><br>It seems many of the {neutral}s in the area have been driven insane.",
             },
             repeat: {},
         },
@@ -140,13 +170,15 @@ const IncidentTypes =
 
     PsychicDrone:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Psychic drone: {Tier} ({gender})",
+        severity: IncidentSeverity.PsychicDrone,
         description:
         {
             main:
             {
-                0: ""
+                0: "Every {gender} hero feels a wave of anxiety and anger!",
+                1: "<br><br>Some distant engine of hatred is stirring. It is projecting a psychic drone onto this battlefield through an orbital amplifier, tuned to only affect {gender}s. For a few days, some people's mood will be quite a bit worse.",
+                2: "<br><br>The drone level is {tier}.",
             },
             repeat: {},
         },
@@ -154,13 +186,14 @@ const IncidentTypes =
 
     PsychicSoothe:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Psychic soothe: {Gender}",
+        severity: IncidentSeverity.PsychicSoothe,
         description:
         {
             main:
             {
-                0: ""
+                0: "Every {gender} hero smiles with contentment!",
+                1: "<br><br>Some distant engine of happiness is stirring. It is projecting a psychic drone onto the battlefield through an orbital amplifier, tuned to only affect {gender}s. For a few days, some people's mood will be quite a bit better.",
             },
             repeat: {},
         },
@@ -168,13 +201,15 @@ const IncidentTypes =
 
     Zzztt:
     {
-        name: "",
-        severity: IncidentSeverity.Neutral,
+        name: "Zzztt...",
+        severity: IncidentSeverity.Bad,
         description:
         {
             main:
             {
-                0: ""
+                0: "One of your towers has had a short circuit, probably because fUCKing DAVE LEFT IT OUT IN THE RAIN.",
+                1: "<br><br>All {power} W of energy stored in the tower has been discharged in a magical explosion.",
+                2: "<br><br>Your tower will be out of service while it performs self-repairs, taking increased external damage.",
             },
             repeat: {},
         },
