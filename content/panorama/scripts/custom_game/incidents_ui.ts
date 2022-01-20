c4cb96efa09a42a0d74c1999e5013e4427fae731
Senyksia <senyksia@gmail.com>
2022-01-20 20:46:33 +1100

    Move incident types to their own file

--- content/panorama/scripts/custom_game/incidents_ui.ts
+++ content/panorama/scripts/custom_game/incidents_ui.ts
@@ -1,28 +1,3 @@
-type LuaArray<T> = Record<number, T>;
-type EventArray<T> = T | LuaArray<T>;
-
-// I feel like I don't need all of these types...
-type ProblemType = keyof typeof ProblemTypes;
-type IncidentType = keyof typeof IncidentTypes;
-type Severity = typeof IncidentSeverity[keyof typeof IncidentSeverity];
-type Description = {main: LuaArray<string>, repeat: LuaArray<string>};
-
-interface SendIncidentLetterEvent {
-    type: IncidentType;
-    targets: EventArray<EntityIndex>;
-    special?: {
-        main?: Record<string, string>,
-        repeat?: Record<string, EventArray<string>>,
-    }
-}
-
-interface UpdateProblemAlarmEvent {
-    type: ProblemType;
-    targets: EventArray<EntityIndex>;
-    major: 0 | 1;
-    increment: 0 | 1;
-}
-
 class UI {
     container: Panel;
     iContainer: Panel;
