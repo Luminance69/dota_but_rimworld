e16dfa3ba14b579ca255c48f4fe072f90bdf2d4d
Luminance <gdezard@icloud.com>
2022-04-07 11:46:25 +1000

    Fixed mad neutral AI not working Also changed it to make the creep not always choose the closest unit, as well as get bored after a little while and switch target

--- game/scripts/vscripts/modifiers/incidents/mad_neutral.lua
+++ game/scripts/vscripts/modifiers/incidents/mad_neutral.lua
@@ -6,21 +6,29 @@ local outposts = {
 }
 
 function modifier_mad_neutral:OnCreated()
-    if IsClient() then return end
-
     self:SetNewTarget()
 
     self:StartIntervalThink(1)
 end
 
 function modifier_mad_neutral:OnIntervalThink()
-    if not self:GetParent():IsAlive() then
+    if not (self:GetParent() and self:GetParent().IsAlive and self:GetParent():IsAlive()) then
         self:Destroy()
         return
     end
 
-    self.bonus_dmg = 25 + Incidents:GetPowerLevel() * 30
-    self.dmg_resist = math.pow(0.7, Incidents:GetPowerLevel()) * -100
+    self.bored = self.bored + 1
+
+    if not (self.target and self.target:IsAlive()) or (1 / RandomFloat(1, self.bored) < 0.1) then
+        self:SetNewTarget()
+    end
+
+    if not (Incidents and Incidents.GetPowerLevel) then return end
+
+    local power_level = Incidents:GetPowerLevel()
+
+    self.bonus_dmg = 25 + power_level * 30
+    self.dmg_resist = math.pow(0.7, power_level) * -100
     self.bonus_speed = 50
 
     if self:GetParent():GetTeamNumber() ~= DOTA_TEAM_NEUTRALS then
@@ -28,20 +36,20 @@ function modifier_mad_neutral:OnIntervalThink()
         self.bonus_dmg = self.bonus_dmg * 0.25
         self.bonus_speed = 10
     end
-
-    if not self.target:IsAlive() then
-        self:SetNewTarget()
-    end
 end
 
 function modifier_mad_neutral:SetNewTarget()
-    local targets = FindUnitsInRadius(DOTA_TEAM_NEUTRALS, self:GetParent():GetAbsOrigin(), nil, -1, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BUILDING, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+    if IsClient() then return end
+
+    self.bored = 0
+
+    local targets = FindUnitsInRadius(DOTA_TEAM_NEUTRALS, self:GetParent():GetAbsOrigin(), nil, -1, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BUILDING, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
 
     while outposts[targets[1]:GetUnitName()] do
         table.remove(targets, 1)
     end
 
-    self.target = targets[1]
+    self.target = targets[RandomInt(1, max(1, math.floor(#targets / 5)))]
 
     self:GetParent():SetForceAttackTarget(self.target)
 end
@@ -53,7 +61,6 @@ function modifier_mad_neutral:IsPurgable() return false end
 
 function modifier_mad_neutral:DeclareFunctions()
     return {
-        MODIFIER_EVENT_ON_ABILITY_FULLY_CAST, -- OnAbilityFullyCast
         MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE, -- GetModifierIncomingDamage_Percentage
         MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE, -- GetModifierPreAttack_BonusDamage
         MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE, -- GetModifierMoveSpeedBonus_Percentage
